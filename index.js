ea37ed78db7804aa35e1f667c8b013b6465c4d56
guyklainer <guyklainer@gmail.com>
2017-09-02 01:26:05 +0300

    add chart

--- index.js
+++ index.js
@@ -1,6 +1,11 @@
 const Trader = require("./lib/trader");
+const DB = require("./lib/db");
+const Router = require("./lib/router");
 
-let trader = new Trader();
+let db = new DB();
+let trader = new Trader(db);
+let router = new Router(db);
+
 interval();
 
 function interval(){
